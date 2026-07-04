07e8a176e24864a33056096b72c177106a43cc9a
srishti-145 <srishtisahu2004@gmail.com>
2026-07-04 09:48:46 +0530

    Time: 43 ms (8.88%), Space: 47.2 MB (5.77%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -1,7 +1,9 @@
 class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
-        HashMap<Character, Integer> map = new HashMap<>();
+      HashMap<Character, Integer> map = new HashMap<>();
+
         int left = 0;
+        int maxFreq = 0;
         int ans = 0;
 
         for (int right = 0; right < answerKey.length(); right++) {
@@ -9,23 +11,12 @@ class Solution {
             char ch = answerKey.charAt(right);
             map.put(ch, map.getOrDefault(ch, 0) + 1);
 
-            
-            int maxFreq = Math.max(
-                    map.getOrDefault('T', 0),
-                    map.getOrDefault('F', 0)
-            );
+            maxFreq = Math.max(maxFreq, map.get(ch));
 
             while ((right - left + 1) - maxFreq > k) {
-
                 char remove = answerKey.charAt(left);
                 map.put(remove, map.get(remove) - 1);
                 left++;
-
-                
-                maxFreq = Math.max(
-                        map.getOrDefault('T', 0),
-                        map.getOrDefault('F', 0)
-                );
             }
 
             ans = Math.max(ans, right - left + 1);
